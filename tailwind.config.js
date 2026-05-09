9f6f958a2e1e8ed74bdc148f112d14020373ef68
Anastasija Mitova <mitova.a24097@next.edu.mk>
2026-05-10 01:42:34 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        kids: ['"Baloo 2"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
